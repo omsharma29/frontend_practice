108412c714f9c1bf9371d8a1e5c02e34b8b9f639
omsharma29 <151984518+omsharma29@users.noreply.github.com>
2024-08-27 09:43:11 +0530

    Fully Completed

--- vite-project/src/Layout/Header.tsx
+++ vite-project/src/Layout/Header.tsx
@@ -1,26 +1,85 @@
+// import 'boxicons/css/boxicons.min.css';
+// import StartHereButton from './component/StartHereButton';
+
+// const Header = () => {
+//   return (
+//     <>
+//     {/* <div className="flex justify-evenly pt-7  ">
+//         <div className="flex ">
+//             <img src="https://rb.gy/pngzb7" alt="Bonsai" />
+//         </div>
+//         <div className="flex gap-5  ">
+//             <div className="text-darkgrey text-[20px]">Product <i className='bx bx-chevron-down'></i></div>
+//             <div className="text-darkgrey text-[20px]">Template <i className='bx bx-chevron-down'></i></div>
+//             <div className="text-darkgrey text-[20px]">Pricing</div>
+//             <div className="text-darkgrey text-[20px]">Reviews</div>
+//         </div>
+//         <div className="flex gap-4 ">
+//             <button className='border-2 border-green px-6  text-green text-[13px] font-bold rounded hover:text-white hover:bg-green'>LogIn</button> <br/>
+//             <StartHereButton/>
+//         </div>
+//     </div> */}
+    
+//     </>
+//   )
+// }
+
+// export default Header
+
 import 'boxicons/css/boxicons.min.css';
+import { useState } from 'react';
 import StartHereButton from './component/StartHereButton';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <div className="flex justify-evenly pt-7  ">
-        <div className="flex ">
-            <img src="https://rb.gy/pngzb7" alt="Bonsai" />
+      <div className=" sticky top-0 bg-white z-50 shadow-b flex justify-between items-center pt-7 px-4 md:px-8 py-4">
+        <div className="flex">
+          <img src="https://rb.gy/pngzb7" alt="Bonsai" />
         </div>
-        <div className="flex gap-5  ">
-            <div className="text-darkgrey text-[20px]">Product <i className='bx bx-chevron-down'></i></div>
-            <div className="text-darkgrey text-[20px]">Template <i className='bx bx-chevron-down'></i></div>
-            <div className="text-darkgrey text-[20px]">Pricing</div>
-            <div className="text-darkgrey text-[20px]">Reviews</div>
+        <div className="hidden md:flex gap-5">
+          <div className="text-darkgrey text-[20px]">Product <i className='bx bx-chevron-down'></i></div>
+          <div className="text-darkgrey text-[20px]">Template <i className='bx bx-chevron-down'></i></div>
+          <div className="text-darkgrey text-[20px]">Pricing</div>
+          <div className="text-darkgrey text-[20px]">Reviews</div>
         </div>
-        <div className="flex gap-4 ">
-            <button className='border-2 border-green px-6  text-green text-[13px] font-bold rounded hover:text-white hover:bg-green'>LogIn</button> <br/>
-            <StartHereButton/>
+        <div className="hidden md:flex gap-4">
+          <button className='border-2 border-green px-6 text-green text-[13px] font-bold rounded hover:text-white hover:bg-green'>LogIn</button>
+          <StartHereButton />
         </div>
-    </div>
-    </>
-  )
-}
+        <div className="md:hidden">
+          <i className='bx bx-menu text-3xl cursor-pointer' onClick={toggleSidebar}></i>
+        </div>
+      </div>
 
-export default Header
+      {/* Sidebar for medium screens */}
+      <div className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-50 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out`}>
+        <div className="flex justify-between items-center p-4">
+         
+          <i className='bx bx-x text-3xl cursor-pointer' onClick={toggleSidebar}></i>
+        </div>
+        <div className="flex flex-col gap-4 p-4">
+          <div className="text-darkgrey text-[20px]">Product <i className='bx bx-chevron-down'></i></div>
+          <div className="text-darkgrey text-[20px]">Template <i className='bx bx-chevron-down'></i></div>
+          <div className="text-darkgrey text-[20px]">Pricing</div>
+          <div className="text-darkgrey text-[20px]">Reviews</div>
+          <button className='border-2 border-green px-6 text-green text-[13px] font-bold rounded hover:text-white hover:bg-green'>LogIn</button>
+          <StartHereButton />
+        </div>
+      </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-black opacity-50 z-40" onClick={toggleSidebar}></div>
+      )}
+    </>
+  );
+};
+
+export default Header;
--- vite-project/src/Layout/Pricing.tsx
+++ vite-project/src/Layout/Pricing.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
+import AddOn from "./component/AddOn";
 import PriceTag from "./component/PriceTag";
 import PriceTag2 from "./component/PriceTag2";
 import PriceTag3 from "./component/PriceTag3";
 import ToggelBtn from "./component/ToggelBtn";
-import 'boxicons/css/boxicons.min.css';
-import AddOn from "./component/AddOn";
 
 function Pricing() {
     const [isMonthly, setIsMonthly] = useState(false);
@@ -14,7 +13,10 @@ function Pricing() {
 
     return (
         <div className="relative max-w-screen-lg mx-auto">
-            <div className="flex flex-row justify-center gap-5 mx-6 mt-15 p-3">
+            {/* U-shaped background */}
+            {/* <div className="blue-shape"></div> */}
+
+            <div className="flex flex-row justify-center gap-5 mx-6 mt-15 p-3 relative z-10">
                 <div className="mb-2 font-semibold text-darkgrey text-[3rem] font-serif">Plan & Pricing</div>
                 <div className="flex items-center text-darkgrey space-x-2 text-[1.2rem]">
                     <span>Monthly</span>
@@ -24,13 +26,13 @@ function Pricing() {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center relative z-10">
                 <PriceTag isMonthly={isMonthly} />
                 <PriceTag2 isMonthly={isMonthly} />
                 <PriceTag3 isMonthly={isMonthly} />
             </div>
 
-            <div className="">
+            <div className="relative z-10">
                 <div className="relative text-center font-serif font-bold text-darkgrey text-[2rem] pt-10">
                     Supercharge your work with add-ons
                 </div>
